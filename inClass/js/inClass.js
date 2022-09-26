73698feba92014f3b915628e657a21440177e2c1
Tums13 <112028358+Tums13@users.noreply.github.com>
2022-09-26 15:05:30 -0700

    added new functions and doc

--- inClass/js/inClass.js
+++ inClass/js/inClass.js
@@ -4,9 +4,33 @@
 //////color of tunic: green
 //////a number that says how much health they have left: 100
 
-let object = {
-    name = "Andrew",
-    age: "21",
-    color = "green",
-    fighterClass = "brawler",
+let player = {
+    //name 
+    name: "Andrew",
+    //color of tunic
+    color: "green",
+    //health
+    health: 100,
+    //backpack
+    backpack: ["Health Potion" , " Sword"]
+};
+
+//make a function that has one parameter called color
+//it should set players tunic color to wha they pass in is
+
+// EX: changeColor('purple');
+
+function changeColor(color) {
+    player.color = color;
+}
+
+//player needs a backpack
+//backpack is a collection of  things represented by words
+//items in backpack: 'health potion' , 'sword', 'cookies'
+
+//part 1: in player object add a propertie called backpack and make it have a 'health potion' and a 'sword'
+//part 2: make a function to add things inside the backpack and use the newItem as a parameter
+
+function addItemToBackpack(newItem) {
+    player.backpack.push(newItem);
 }
