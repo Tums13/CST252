79577f40917a7dc797156bedcf3f5a5462883c40
Tums13 <112028358+Tums13@users.noreply.github.com>
2022-09-26 15:50:00 -0700

    adding js to show results

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -1,5 +1,19 @@
-function onLoad() {
+/**
+ * Author:    Andrew Tamez
+ * Created:   9.26.2022
+ * 
+ * (c) Copyright by Tamez Codes.
+ **/
 
-console.log('it worked.');
+let number = [1, 3, 4, 8, 9];
 
+let isOdd = (number) => {
+    return number % 2 != 0;
 }
+
+console.log("Is 1 odd?" , isOdd(1));
+console.log("Is 2 odd?" , isOdd(2));
+
+var results = number.map(isOdd);
+
+console.log("Test of oddness on the array of numbers: ", results);
