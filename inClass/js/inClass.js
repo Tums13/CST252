fd4d87de8ab7c9085014d8bfac67e4892d12dfdc
Tums13 <112028358+Tums13@users.noreply.github.com>
2022-09-26 14:28:19 -0700

    adding properties to object

--- inClass/js/inClass.js
+++ inClass/js/inClass.js
@@ -1,1 +1,12 @@
 //make a variable that has an object in it
+//Object is keeping track of 3 propotioes / ietms
+//////The name of player: Andrew
+//////color of tunic: green
+//////a number that says how much health they have left: 100
+
+let object = {
+    name = "Andrew",
+    age: "21",
+    color = "green",
+    fighterClass = "brawler",
+}
