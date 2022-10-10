2cdeb302a2ba299d86f6e59c78d297e6cbb7b1a6
Tums13 <112028358+Tums13@users.noreply.github.com>
2022-10-10 15:31:11 -0700

    finished lab10

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -1,48 +1,77 @@
-// 1. What element triggers the event? (button)
-// 2. What's the actual event? (click, mousing over)
-// 3. What happens when the event happens (something shows up)
-
-// 1. itembox div
-// 2. click on div
-// 3. sidepanel appears
-
-
 // Selecting element to trigger the event
 let button = document.querySelector(".itembox");
+let button2 = document.querySelector(".itembox2");
+var image = document.querySelector(".courseimage");
 
-// Making the event - add a border and change course description
+//anon function to update style
+//all updated information for info panel when button is clicked / pressed
 let buttonPress = () => {
+    //infopanel is displayed
     infopanel.style.display = "block";
+    //updates tite
     let courseInfo = document.querySelector(".descriptionitem")
     courseInfo.innerHTML = "CST 252 Scripting for Multimedia";
+    //updates course description
     let courseDesc = document.querySelector("#courseDescription");
-    courseDesc.innerHTML = "Here is the description of the course";
+    courseDesc.innerHTML = "A course about learning javaScript";
+    //updates image
+    image.src = "images/cst252.jpg";
+    console.log(image);
+};
+
+//anon function to update style
+//all updated information for info panel when button is clicked / pressed
+let buttonPress2 = () => {
+    //infopanel is displayed
+    infopanel.style.display = "block";
+    //updates tite
+    let courseInfo = document.querySelector(".descriptionitem")
+    courseInfo.innerHTML = "CST 351 Web Design";
+    //updates course description
+    let courseDesc = document.querySelector("#courseDescription");
+    courseDesc.innerHTML = "A course about learning web development";
+    //updates image
+    image.src = "images/cst351.jpg";
 };
 
 // Creating Event listener to trigger the event when course is clicked
 button.addEventListener("click", buttonPress);
+button2.addEventListener("click", buttonPress2);
 
 //  Selecting elements to trigger the event
 let exitButton = document.querySelector("#exitbutton");
 let infopanel = document.querySelector("#infopanel");
 
 // Making the event -- element dissapears when the event happens
+
+// disables display of info panel when exit button is clicked
 let exitClick = () => {
     infopanel.style.display = "none";
 }
 
+//displays thin border when mouse goes "out" / away from button 
 let out = () => {
     button.style.border = "thin grey solid";
 }
 
+//displays thin border when mouse goes "in" / towards button 
 let inside = () => {
     button.style.border = "5px solid black";
 }
+//displays thin border when mouse goes "out" / away from button 2
+let out2 = () => {
+    button2.style.border = "thin grey solid";
+}
+//displays thin border when mouse goes "in" / towards button 2
+let inside2 = () => {
+    button2.style.border = "5px solid black";
+}
 
+//event listeners for inside and out style events
 button.addEventListener("mouseout", out);
 button.addEventListener("mouseover", inside);
+button2.addEventListener("mouseout", out2);
+button2.addEventListener("mouseover", inside2);
 
 // Creating event listener to trigger the event (click)
 exitButton.addEventListener("click", exitClick, out);
-
-//button 2
