b4f62adc1f3922636c2deaa71c22dc217c896721
Tums13 <112028358+Tums13@users.noreply.github.com>
2022-11-07 15:12:14 -0800

    finalized and working lab14

--- lab14/lab14/js/lab.js
+++ lab14/lab14/js/lab.js
@@ -1,28 +1,24 @@
-document.addEventListener("DOMContentLoded", () => {
+document.addEventListener("DOMContentLoaded", () => {
 
 
 	//putting the input box into a variable
-	let inputBox = document.querySelectorAll('guessingBox');
-
-
+	let inputBox = document.querySelector('.guessingBox'); //added period to identify class and removed ALL from querySelector
 	//input event - what should happen:
 	////check if they entered the right number
 	////if so
 	/////////create a new div
 	/////////set the div to have a message "you won!"
-	let inputHandler = () => {
-		if (+e.currentTarget.value === 6) {
+	let inputHandler = (number) => { //passed inputBox into the anon using number
+		if (+number.currentTarget.value === 6) { // added + to make number a integer
 			let newBox = document.createElement('div');
-			div.classList.add('.box');
-			div.text = 'You won!';
+			newBox.classList.add("box"); //remove period from class name wasn't needed
+			newBox.textContent = "You won!"; //changed text to textContent
+			document.body.appendChild(newBox); //appended and updated the newBox to document
+
 
 		}
 	}
 
-	inputBox.addEventListener('input', inputBox);
-
-
-
-
+	inputBox.addEventListener('input', inputHandler); //changed function use to inputHandler
 
 });
